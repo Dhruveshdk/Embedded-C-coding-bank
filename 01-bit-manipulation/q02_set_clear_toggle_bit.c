e09e348546bc6529ce05eff23c85f478b3127368
Dhruvesh Kamble <kamble.dhruveshdk@gmai.com>
2026-06-26 03:49:10 +0530

    Added q02 SetClearToggleBit

---
+++ 01-bit-manipulation/q02_set_clear_toggle_bit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int SetFifthBit(int n) {
+    n = n | (1<<5);
+    return n;
+}
+
+int ClearFifthBit(int n) {
+    n = n & ~(1<<5);
+    return n;
+}
+
+int toggleFifthBit(int n) {
+    n = n ^ (1<<5);
+    return n;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", SetFifthBit(n));
+    printf("%d", ClearFifthBit(n));
+    printf("%d", toggleFifthBit(n));
+    return 0;
+}
