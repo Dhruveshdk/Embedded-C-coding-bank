dc105c1b8a2d7daefa60a3b0db6b70e96572e5b4
Dhruvesh Kamble <kamble.dhruveshdk@gmai.com>
2026-06-26 03:28:08 +0530

    Added q03 Count Set bits

---
+++ 01-bit-manipulation/q03_count_set_bits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int countSetBits(unsigned int n) {
+    unsigned int cnt;
+    while(n){
+        n = n & (n-1);
+        cnt++;
+    }
+    return cnt;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", countSetBits(n));
+    return 0;
+}
